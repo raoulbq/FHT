d7c6746877939c6b1a8855b2d127d3ae0e6bdee5
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2016-03-03 15:15:26 +0100

    Parameters

--- FHT.c
+++ FHT.c
@@ -15,28 +15,7 @@
 
 #include "lalgebra.h"
 #include "FHT.h"
-
-// Recursion parameters
-#define PI 3.1415926535897932385
-#define BIGN 128
-#define LITN (BIGN*5)
-
-#define BIGC sqrt((double)2*BIGN + 1)
-
-#define ALPHA (2.0 / BIGC)
-#define BETA 0
-#define GAMMA (-1.0)
-
-#define D0 ((double)1.0 / pow(PI, (double)1.0/4.0))
-
-// Define al, bl, cl, ul, vl and wl
-#define AL(l) (double)sqrt((double)2.0 / (l+1.0))
-#define BL(l) (double)0
-#define CL(l) (double)((-1.0) * sqrt((double)l / (l+1.0)))
-
-#define UL(l) (double)(AL(l) / ALPHA)
-#define VL(l) (double)(BL(l) + (-1) * (UL(l) * BETA))
-#define WL(l) (double)((-1) * GAMMA * UL(l))
+#include "parameters.h"
 
 
 // Defines the data points
