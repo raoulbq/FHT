d774619ace4049be073fe39855934a7e93d18cdf
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2016-03-03 17:59:58 +0100

    Remove unnecessary variables

--- main.c
+++ main.c
@@ -14,24 +14,22 @@
 
 int main(int argc, char *argv[]) {
   clock_t fclock, nclock;
-  int n = LITN;
-  int N = BIGN;
   int i;
-  double *data = (double*) fftw_malloc(sizeof(double) * (2*n+1));
-  double *naiveResult = (double*) fftw_malloc(sizeof(double) * N);
-  double *fancyResult = (double*) fftw_malloc(sizeof(double) * N);
+  double *data = (double*) fftw_malloc(sizeof(double) * (2*LITN+1));
+  double *naiveResult = (double*) fftw_malloc(sizeof(double) * BIGN);
+  double *fancyResult = (double*) fftw_malloc(sizeof(double) * BIGN);
 
 
   // Load input data
-  oneDFileReader("data/input.txt", (2*n+1), data);
-  for(i=0; i<=2*n; ++i) {
+  oneDFileReader("data/input.txt", (2*LITN+1), data);
+  for(i=0; i<=2*LITN; ++i) {
     data[i] *= exp(0 - pow(xk(i), 2) / 2);
   }
 
 
   // Perform transformations
-  initFastFouriers(N);
-  initRns(N);
+  initFastFouriers(BIGN);
+  initRns(BIGN);
 
   nclock = clock();
   naiveTransform(data, naiveResult);
@@ -41,7 +39,7 @@ int main(int argc, char *argv[]) {
   oneDTransform(data, fancyResult);
   fclock -= clock();
 
-  destroyFastFouriers(N);
+  destroyFastFouriers(BIGN);
 
 
   // Save output data
@@ -50,9 +48,9 @@ int main(int argc, char *argv[]) {
   resultsn = fopen("data/result_n.txt", "wt+");
   resultsf = fopen("data/result_f.txt", "wt+");
 
-  for(i=0; i<N; ++i){
-    fancyResult[i] *= (2*BIGC) / n;
-    naiveResult[i] *= (2*BIGC) / n;
+  for(i=0; i<BIGN; ++i){
+    fancyResult[i] *= (2*BIGC) / LITN;
+    naiveResult[i] *= (2*BIGC) / LITN;
     fprintf(resultsn, "%.16lf\n", naiveResult[i]);
     fprintf(resultsf, "%.16lf\n", fancyResult[i]);
   }
